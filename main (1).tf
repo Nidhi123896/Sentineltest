866f1d7cb1793ea528fe6a42771f4247afc56e40
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-14 17:25:42 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,8 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAZEVC2EMM37RPTRIY"
-  secret_key = "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"
+  
 }
 
 resource "aws_s3_bucket" "s3bucketsentnidhi" {
