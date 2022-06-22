92ecc3ce7e243ef9eb93b4fca255e2c72ed90310
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-22 10:31:20 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,18 +1,102 @@
-# Configure the AWS Provider
-provider "aws" {
-  region     = "us-west-2"
+NIdhi task-
+
+
+provider aws {
+  region     = us-east-1
   
 }
 
-resource "aws_s3_bucket" "s3bucketsentnidhi" {
-  bucket = "s3bucketsentnidhi"
-  tags = {
-    Name        = "My bucketone"
-    Environment = "Trial"
-  }
-}
-resource "aws_s3_bucket_public_access_block" "s3bucketsentnidhi" {
-  bucket = aws_s3_bucket.s3bucketsentnidhi.id
-  block_public_acls   = true
+resource aws_kms_key my_kms_key {
+  description         = My KMS Keys for Data Encryption
+  customer_master_key_spec = SYMMETRIC_DEFAULT
+  is_enabled               = true
+  #enable_key_rotation      = var.rotation_enabled  
 
+  tags = {
+    Name = my_kms_key
+  }
+
+  policy = EOF
+{
+    Id key-consolepolicy-3,
+    Version 2012-10-17,
+    Statement [
+        {
+            Sid Enable IAM User Permissions,
+            Effect Allow,
+            Principal {
+                AWS arnawsiam390132021439userchandraaws539
+            },
+            Action kms,
+            Resource 
+        },
+        {
+            Sid Allow access for Key Administrators,
+            Effect Allow,
+            Principal {
+                AWS arnawsiam390132021439userchandraaws539
+            },
+            Action [
+                kmsCreate,
+                kmsDescribe,
+                kmsEnable,
+                kmsList,
+                kmsPut,
+                kmsUpdate,
+                kmsRevoke,
+                kmsDisable,
+                kmsGet,
+                kmsDelete,
+                kmsTagResource,
+                kmsUntagResource,
+                kmsScheduleKeyDeletion,
+                kmsCancelKeyDeletion
+            ],
+            Resource 
+        },
+        {
+            Sid Allow use of the key,
+            Effect Allow,
+            Principal {
+                AWS arnawsiam390132021439userchandraaws539
+            },
+            Action [
+                kmsEncrypt,
+                kmsDecrypt,
+                kmsReEncrypt,
+                kmsGenerateDataKey,
+                kmsDescribeKey
+            ],
+            Resource 
+        },
+        {
+            Sid Allow attachment of persistent resources,
+            Effect Allow,
+            Principal {
+                AWS arnawsiam390132021439userchandraaws539
+            },
+            Action [
+                kmsCreateGrant,
+                kmsListGrants,
+                kmsRevokeGrant
+            ],
+            Resource ,
+            Condition {
+                Bool {
+                    kmsGrantIsForAWSResource true
+                }
+            }
+        }
+    ]
 }
+EOF
+}
+
+resource aws_kms_alias my_kms_alias {
+  target_key_id = aws_kms_key.my_kms_key.key_id
+  #name          = alias${var.kms_alias}
+}
+
+output key_id {
+  value = aws_kms_key.my_kms_key.key_id
+}}
