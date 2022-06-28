dfa49f01de46e95d1b515aca044f80ac5a491cea
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-28 13:22:17 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -3,16 +3,21 @@ provider "aws" {
   region     = "us-west-2"
 }
 
-resource "aws_instance" "ec2testtf" {
+resource "aws_instance" "ec2test" {
+   
     ami = "ami-0ca285d4c2cda3300"
+    availability_zone = "us-west-2a"
     instance_type = "t2.micro"
+    tags = {
+    Name = "instance1"
+  }
 }
 resource "aws_ebs_volume" "data-vol" {
  availability_zone = "us-west-2a"
  size = 1
 }
 resource "aws_volume_attachment" "vol" {
- availability_zone = "us-west-2a"
+
  device_name = "/dev/sdc"
  volume_id = aws_ebs_volume.data-vol.id
  instance_id = aws_instance.ec2testtf.id
