e73e44f91bdb9e2b79dd28ccf01bdad17c0b50f7
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-28 13:02:10 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -7,3 +7,15 @@ resource "aws_instance" "ec2testtf" {
     ami = "ami-0ca285d4c2cda3300"
     instance_type = "t2.micro"
 }
+resource "aws_ebs_volume" "data-vol" {
+ availability_zone = "us-west-2a"
+ size = 1
+}
+resource "aws_volume_attachment" "vol" {
+ device_name = "/dev/sdc"
+ volume_id = aws_ebs_volume.data-vol.id
+ instance_id = aws_instance.inst.id
+}
+resource "aws_ebs_snapshot" "example_snapshot" {
+  volume_id = aws_ebs_volume.data-vol.id
+}
