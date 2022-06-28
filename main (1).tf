c2392f0f10fdcdc8ccd83a0bee4fde98e8cedd81
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-28 11:45:27 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -4,11 +4,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2" {
-  ami               = "ami-5b673c34"
+  ami               = "ami-0cff7528ff583bf9a"
   instance_type     = "t2.micro"
 }
 resource "aws_ebs_volume" "data-vol" {
- availability_zone = "us-east-1"
+ availability_zone = "us-east-1a"
  size = 1
 
 
