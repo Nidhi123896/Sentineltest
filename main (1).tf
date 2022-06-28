591456ea114f5c4a5a30eb33d323fb35bdef4bad
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-28 15:36:05 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -34,7 +34,7 @@ resource "aws_volume_attachment" "vol" {
 }
 resource "aws_ebs_snapshot" "example_snapshot" {
   volume_id = aws_ebs_volume.data-vol.id
-  encrypted = true
+ 
 
   tags = {
     Name = "Encryption check for snapshot"
