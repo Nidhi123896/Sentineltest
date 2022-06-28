80a0a478d5287327d1613a18d1b32ff154d7e570
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-28 14:47:12 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -19,10 +19,7 @@ resource "aws_kms_key" "ebs_encryption" {
 resource "aws_ebs_volume" "data-vol" {
  availability_zone = "us-west-2a"
  size = 1
- kms_key_id = aws_kms_key.ebs_encryption.arn
- tags = {
-    Name = "Encryption check for volume"
-  }
+
 }
 resource "aws_volume_attachment" "vol" {
 
@@ -32,7 +29,10 @@ resource "aws_volume_attachment" "vol" {
 }
 resource "aws_ebs_snapshot" "example_snapshot" {
   volume_id = aws_ebs_volume.data-vol.id
-  
+  kms_key_id = aws_kms_key.ebs_encryption.arn
+  tags = {
+    Name = "Encryption check for snapshot"
+  }
 }
 
 
