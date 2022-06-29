30cfa621f09bbf8e11004f4f20a098d3606b3a3a
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-06-29 13:32:18 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -24,8 +24,7 @@ resource "aws_ebs_default_kms_key" "example" {
 resource "aws_ebs_volume" "data-vol" {
  availability_zone = "us-west-2a"
  size = 1
-
-#kms_key_id = aws_ebs_default_kms_key.example.arn
+ kms_key_id = aws_kms_key.ebs_encryption.arn
  tags = {
     Name = "Encryption check for volume"
   }
