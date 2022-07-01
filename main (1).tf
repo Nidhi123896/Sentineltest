498e83eb19300954901a729412fd83c1fc11c223
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-01 12:42:49 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,3 +1,40 @@
+resource "aws_iam_role" "example" {
+  name = "eks-cluster-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.1.0.0/16"
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.mainvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+
 resource "aws_cloudwatch_log_group" "nidhi" {
   name = "loggp"
 
@@ -12,7 +49,17 @@ variable "cluster_name" {
 }
 
 resource "aws_eks_cluster" "example" {
-  depends_on = [aws_cloudwatch_log_group.example]
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.main.id]
+  }
+  
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy,
+    aws_cloudwatch_log_group.example,
+  ]
+
 
   enabled_cluster_log_types = ["api", "audit","authenticator","controllerManager","scheduler"]
   name                      = var.cluster_name
