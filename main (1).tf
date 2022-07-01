4065253432e9ff2bfcc250063026365b4aa532f6
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-01 12:23:55 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -24,5 +24,5 @@ resource "aws_cloudwatch_log_group" "example" {
  
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = 7
-  enabled_cluster_log_types =  true
+  
 }
