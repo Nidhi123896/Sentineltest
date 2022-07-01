883b9a0046f225ca09d2dbdb04e30cc464591658
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-01 13:05:28 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -34,7 +34,7 @@ resource "aws_vpc" "mainvpc" {
 }
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.mainvpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.1.1.0/24"
  
   tags = {
     Name = "Main"
