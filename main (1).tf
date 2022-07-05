d25a99ca02db39b52957ef40aad196756dfda7bb
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-05 15:36:27 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,1 +1,28 @@
+resource "aws_vpc" "mainvpc" {
+  cidr_block = "10.1.0.0/16"
+}
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.mainvpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+   
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
