3335e9fc86c9bcd7da984d2d3133aa166aad3606
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-05 16:14:00 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "allow_tls" {
     protocol         = "-1"
    
   }
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+   
+  }
+
 
   tags = {
     Name = "allowtls"
