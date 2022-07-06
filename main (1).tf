6e4623b0f0c55218813eb00d767854fbd237b615
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-06 10:06:11 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 
@@ -14,8 +14,8 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 445
-    to_port          = 445
+    from_port        = 23
+    to_port          = 23
     protocol         = "tcp"
     self            = true
     cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
@@ -27,7 +27,77 @@ resource "aws_security_group" "allow_tls" {
     self            = true
     cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
    }
+   ingress {
+    from_port        = 135
+    to_port          = 135
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 445
+    to_port          = 445
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 3389
+    to_port          = 3389
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 5500
+    to_port          = 5500
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 1433
+    to_port          = 1433
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 4333
+    to_port          = 4333
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 5432
+    to_port          = 5432
+    protocol         ="tcp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
+   ingress {
+    from_port        = 1434
+    to_port          = 1434
+    protocol         ="udp"
+    self            = true
+    cidr_blocks      = [aws_vpc.mainvpc.cidr_block]
+   }
     tags = {
-    Name = "allowtls"
+    Name = "ingressrule"
   }
+}
+
+
+resource "aws_instance" "my-ec2" {
+  ami = "ami-0ca285d4c2cda3300"
+  instance_type = "t2.micro"
+  security_groups = ["allow_tls"]
 }
