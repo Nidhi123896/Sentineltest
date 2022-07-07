75209e1d868505f367ad4805cc9f34076f4e881f
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-07 16:17:20 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -8,7 +8,7 @@ resource "aws_subnet" "main2" {
   vpc_id     = aws_vpc.vpcsg.id
   cidr_block = "10.1.1.0/24"
   
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = us-east-1a
  
   tags = {
     Name = "subnet1"
@@ -18,7 +18,7 @@ resource "aws_subnet" "main3" {
   vpc_id     = aws_vpc.vpcsg.id
   cidr_block = "10.1.2.0/24"
  
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = us-east-1b
   tags = {
     Name = "subnet2"
   }
