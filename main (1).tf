47098c5599d65352fa4faebc360cd343b39b130a
Demo12344 <86946343+Demo12344@users.noreply.github.com>
2022-07-11 11:25:54 +0530

    Update main (1).tf

--- main (1).tf
+++ main (1).tf
@@ -1,16 +1,13 @@
-provider "aws" {
-  region = "us-east-1"
+provider "aws"{
+  region= "us-west-2"
 }
 resource "aws_vpc" "vpcsg" {
   cidr_block = "10.0.0.0/16"
-  
 }
 resource "aws_subnet" "mainone" {
   vpc_id     = aws_vpc.vpcsg.id
   cidr_block = "10.0.1.0/24"
-  
-  availability_zone ="us-east-1c"
- 
+  availability_zone ="us-west-2b"
   tags = {
     Name = "subnet1"
   }
@@ -18,140 +15,21 @@ resource "aws_subnet" "mainone" {
 resource "aws_subnet" "maintwo" {
   vpc_id     = aws_vpc.vpcsg.id
   cidr_block = "10.0.2.0/24"
- 
-  availability_zone = "us-east-1d"
+  availability_zone = "us-west-2d"
   tags = {
     Name = "subnet2"
   }
 }
-
-resource "aws_security_group" "allow_tlss" {
-  name        = "allow"
-  description = "Allow TLS inbound traffic"
- vpc_id      = aws_vpc.vpcsg.id
-  
-  tags = {
-    Name = "ingress"
-  }
+resource "aws_autoscaling_group" "bar" {
+  name                      = "foobar3-terraform-test"
+  availability_zones = ["us-west-2b","us-west-2d"]
+  max_size                  = 1
+  min_size                  = 0
+  launch_configuration       = aws_launch_configuration.as_conf.name
 }
-
-
-resource "aws_instance" "my-ec2" {
-  ami ="ami-087c17d1fe0178315"
+resource "aws_launch_configuration" "as_conf" {
+  image_id      = "ami-0d70546e43a941d70"
   instance_type = "t2.micro"
- 
-  #role= "aws_iam_role.EC2S3TF1.name"
-  #iam_instance_profile = [aws_iam_instance_profile.EC2S3TF1.name]
-  vpc_security_group_ids = ["${aws_security_group.allow_tlss.id}"]
-   subnet_id = aws_subnet.maintwo.id
-  tags = {
-    ec2_create = "instance1"
-  }
-}
-
-
-
-
-
-resource "aws_security_group_rule" "rule1" {
-  type              = "ingress"
-  from_port         = 1434
-  to_port           = 1434
-  protocol          = "udp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule2" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 5432
-  to_port           = 5432
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule3" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule4" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 4333
-  to_port           = 4333
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule5" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 1433
-  to_port           = 1433
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule6" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 5500
-  to_port           = 5500
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule7" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 445
-  to_port           = 445
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule8" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 3389
-  to_port           = 3389
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-resource "aws_security_group_rule" "rule9" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 135
-  to_port           = 135
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-
-resource "aws_security_group_rule" "rule10" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
-}
-
-resource "aws_security_group_rule" "rule11" {
-  type              = "ingress"
-  #self              = true
-  from_port         = 23
-  to_port           = 23
-  protocol          = "tcp"
-  cidr_blocks       = ["10.1.0.0/16"]
-  security_group_id = aws_security_group.allow_tlss.id
 }
 
 
